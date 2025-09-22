5461e48ba8e3a9d32e8b16757798edb9ee2be07b
Fajar ZafarLabs <fajar@zafarlabs.com>
2025-09-22 23:03:41 +0700

    Refactor DashboardPage layout and styles; enhance card components with icons and improve overall structure

--- src/features/agent/pages/DashboardPage.tsx
+++ src/features/agent/pages/DashboardPage.tsx
@@ -2,9 +2,9 @@ import { useEffect } from "react";
 import { Typography } from "antd";
 import { useAppDispatch } from "@/hooks/redux";
 import { setSmallTitle } from "@/store/layoutSlice";
-import { Card, Row, Col } from "antd";
+import { Card, Row, Col, Tooltip, Button } from "antd";
+import { MoreOutlined, ReloadOutlined } from "@ant-design/icons";
 
-const { Title, Paragraph } = Typography;
 const gutter: any = [16, { xs: 12, sm: 16, md: 20, lg: 24 }];
 
 export default function DashboardPage() {
@@ -12,7 +12,6 @@ export default function DashboardPage() {
 
   useEffect(() => {
     dispatch(setSmallTitle("Dashboard Agent"));
-
     // Reset title on component unmount
     return () => {
       dispatch(setSmallTitle("Dashboard"));
@@ -22,37 +21,44 @@ export default function DashboardPage() {
   return (
     <div className="dashboard-grid">
       <Row gutter={gutter}>
-        {/* Kolom kiri besar (Profile + daftar kecil) */}
+
         <Col xs={24} lg={10} xl={8}>
-          <Card className="card card--profile" />
-          <Card className="card card--list mt" />
-          <Card className="card card--list mt" />
+          <Card className="noborderHeader" extra={<a href="#"> <MoreOutlined /> </a>} title="My Profile">
+            Profile
+          </Card>
         </Col>
 
-        {/* Kolom tengah (stat kecil + list besar) */}
-        <Col xs={24} lg={14} xl={10}>
+        <Col xs={24} lg={14} xl={16}>
           <Row gutter={gutter}>
-            <Col xs={24} sm={8}>
-              <Card className="card card--stat" />
+            <Col xs={24} lg={8} xl={8}>
+              <Card className="noborderHeader" extra={<a href="#"> <ReloadOutlined /> </a>} title="Total Calls">
+                ...
+              </Card>
             </Col>
-            <Col xs={24} sm={8}>
-              <Card className="card card--stat" />
+            <Col xs={24} lg={8} xl={8}>
+              <Card className="noborderHeader" extra={<a href="#"> <ReloadOutlined /> </a>} title="Missed Calls">
+                ...
+              </Card>
             </Col>
-            <Col xs={24} sm={8}>
-              <Card className="card card--stat" />
+            <Col xs={24} lg={8} xl={8}>
+              <Card className="noborderHeader" extra={<a href="#"> <ReloadOutlined /> </a>} title="AVG. Waiting Time">
+                ...
+              </Card>
             </Col>
 
-            <Col xs={24} className="mt">
-              <Card className="card card--listTall" />
+            <Col xs={24} lg={12} xl={12}>
+              <Card className="noborderHeader" extra={<a href="#"> <ReloadOutlined /> </a>} title="All My Case">
+                ...
+              </Card>
+            </Col>
+            <Col xs={24} lg={12} xl={12}>
+              <Card className="noborderHeader" extra={<a href="#"> <ReloadOutlined /> </a>} title="Talk / Listen Ration">
+                ....
+              </Card>
             </Col>
           </Row>
         </Col>
 
-        {/* Kolom kanan (chart besar + sentiment bawah) */}
-        <Col xs={24} xl={6}>
-          <Card className="card card--chartTall" />
-          <Card className="card card--chart mt" />
-        </Col>
       </Row>
     </div>
   );
